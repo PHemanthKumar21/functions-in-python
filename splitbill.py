ea537761621e9ccf481535d79ae1166981608576
PHemanthKumar21 <poolahemanthnaidu@gmail.com>
2026-07-28 21:24:34 +0530

    Add files via upload

---
+++ splitbill.py
@@ -0,0 +1,29 @@
+#splitbill
+def splitbill():
+    a=int(input("enter the no.of friends"))
+    b=int(input("enter the amount"))
+    print("per head bill is",b//a)
+splitbill()
+
+
+
+'''def splitbill():
+    a=int(input("enter the no.of friends"))
+    b=int(input("enter the amount"))
+    print(f"per head bill is {b//a}")
+    print("per head bill is {}".format(b//a))
+splitbill()'''
+
+
+'''def splitbill():
+    a=int(input("enter the no.of friends"))
+    b=int(input("enter the amount"))
+    c=b//a
+    print(f"per head bill is {c}")
+    print("per head bill is {}".format(c))
+splitbill()'''
+
+
+
+
+                
