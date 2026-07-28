46f58b0ddbc4171ff117c560d38bb695e3150562
PHemanthKumar21 <poolahemanthnaidu@gmail.com>
2026-07-28 21:23:27 +0530

    Add files via upload

---
+++ keyword and positional arguments.py
@@ -0,0 +1,28 @@
+#keyword and positional arguments
+'''def details(id,name,mailid):
+    id=10
+    name="hemanth" 
+    mailid="hemanth@gmail.com"           #OUTPUT: 10 hemanth hemanth@gmail.com
+    print(id,name,mailid)
+details(id="id",name="hemanth",mailid="hemanth@gmail.com")'''
+
+
+
+
+'''def details(id,name,mailid):
+    print(id,name,mailid)
+details(id="id",name="name",mailid="mailid")
+details(id=20,name="hemanth",mailid="hemanth@gmail.com")
+details(id=30,name="sanjay sahuu",mailid="sahu@gmail.com")
+details(40,"jay","jay@gmail.com")
+details("siddu","siddu@gmail.com",50)
+details(name="ram",mailid="ram@gmail.com",id=4)'''
+
+
+
+
+
+
+
+
+
